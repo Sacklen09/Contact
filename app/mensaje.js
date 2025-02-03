9452fb61167757ae9c044509e277fd8999862539
Sacklen09 <isaacdcm09@gmail.com>
2025-02-03 11:45:27 -0600

    primer commit

---
+++ app/mensaje.js
@@ -0,0 +1,35 @@
+document.getElementById("enviar") . 
+addEventListener("click" , 
+    function() { 
+        let formulario = document.getElementById("contactForm"); 
+        //poner este mensaje de envio de formulario despues de llenarlo y enviarlo, tambien darle estilo al texto y centrarlo
+        formulario.style.textAlign = "center";
+        formulario.style.padding = "20px";
+        formulario.style.border = "1px solid black";
+        formulario.style.marginTop = "20px";
+        formulario.style.marginBottom = "20px";
+        //mostrar mensaje de envio de formulario
+        formulario.innerHTML = "<p>Message sent successfully!</p>";
+        //cambiar el color del texto
+        formulario.style.color = "Black";
+        formulario.style.backgroundColor = "White";
+
+       
+
+        
+
+        //limpiar el formulario
+        document.getElementById("contactForm").reset();
+
+    
+        //esperar 3 sgundos para ocultar el mensaje
+        setTimeout (() => {
+            window.location.href = "index.html"; //cambiar a la página de inicio
+        
+        }, 3000);
+    
+    
+            
+               });
+
+
