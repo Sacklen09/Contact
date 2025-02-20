4fbe69091f94d6c0fea0ab351becbd3d13634111
Sacklen09 <isaacdcm09@gmail.com>
2025-02-20 11:41:49 -0600

    Update mensaje.js

--- app/mensaje.js
+++ app/mensaje.js
@@ -1,14 +1,7 @@
 //limpiar el formulario
-//document.getElementById("contactForm").reset();
+document.getElementById("contactForm").reset();
 
-//usar el unload para redireccionar a otra pagina
-//window.onunload = function() {
-  //window.location.href = "index.html";
-//}
-//esperar 3 segundos para redireccionar
-//setTimeout(function(){
-  //window.location.href = "index.html";
-//}, 3000);
+
 
 
   
